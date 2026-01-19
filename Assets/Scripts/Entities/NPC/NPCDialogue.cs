9d67bfed2f23966386b18b2e02ea037c030b197d
DoubleMii <sebas.diaz.lugo@gmail.com>
2026-01-19 23:55:43 +0100

    lo intente lol
    aaaa

--- Assets/Scripts/Entities/NPC/NPCDialogue.cs
+++ Assets/Scripts/Entities/NPC/NPCDialogue.cs
@@ -3,21 +3,41 @@ using UnityEngine;
 
 public class NPCDialogue : MonoBehaviour
 {
-    [SerializeField] List<string> Dialogo;
-    [SerializeField] NPCDialogueUI DialogueUI;
-    [SerializeField] Quests Cegarro;
+    [Header("=== DIÁLOGOS (EDITABLES EN INSPECTOR) ===")]
+    [SerializeField] List<string> dialogoAntesMision;
+    [SerializeField] List<string> dialogoDuranteMision;
+    [SerializeField] List<string> dialogoDespuesMision;
+
+    [Header("=== REFERENCIAS ===")]
+    [SerializeField] NPCDialogueUI dialogueUI;
+    [SerializeField] Quests quest;
+
     public void Hablar()
     {
-        DialogueUI.TextoVisible(Dialogo);
-        DialogueUI.OnFinish += IniciarMisionCegarro;
-        Debug.Log("Hablando con NPC");
+        dialogueUI.OnFinish -= IniciarMision;
+        dialogueUI.OnFinish += IniciarMision;
+
+        if (!quest.QuestStarted)
+        {
+            dialogueUI.TextoVisible(dialogoAntesMision);
+        }
+        else if (!quest.QuestCompleted)
+        {
+            dialogueUI.TextoVisible(dialogoDuranteMision);
+        }
+        else
+        {
+            dialogueUI.TextoVisible(dialogoDespuesMision);
+        }
     }
 
-    public void IniciarMisionCegarro()
+    void IniciarMision()
     {
-        Cegarro.StartQuest();
-        DialogueUI.OnFinish -= IniciarMisionCegarro;
-        Debug.Log("Iniciar mision");
-    }
+        if (!quest.QuestStarted)
+        {
+            quest.StartQuest();
+        }
 
+        dialogueUI.OnFinish -= IniciarMision;
+    }
 }
